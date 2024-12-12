7f70bffcec868311f0c14f7ea74b0e487a9a544c
Joshua Geissler <jgeissler14@gmail.com>
2024-12-12 11:24:49 -0800

    vpc id

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_lb_target_group" "nginx_tg" {
   name     = "nginx-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-12345678"
+  vpc_id   = "vpc-0b5830bd356ede7f1"
 
   health_check {
     interval            = 30
