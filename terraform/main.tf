71dae42038597f2e575efe60232583184dd7e8fd
Joshua Geissler <jgeissler14@gmail.com>
2024-12-12 17:27:11 -0800

    change sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,8 +94,8 @@ resource "aws_lb_target_group" "nginx_tg" {
     interval            = 30
     path                = "/"
     timeout             = 5
-    healthy_threshold   = 1
-    unhealthy_threshold = 1
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
     protocol            = "HTTP"
   }
 
